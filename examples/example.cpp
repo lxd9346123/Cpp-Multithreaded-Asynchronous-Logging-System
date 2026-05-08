fef0a32984ef99657b9626914097c882e45efe77
east <lxd9346123@gmail.com>
2026-05-08 12:02:59 +0800

    [add]support multithread async log

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,7 +1,30 @@
 #include "Logger.h"
 
+void log_thread1(){
+    int i = 0;
+    for (int j = 0; j < 10; j ++){
+        i ++;
+        mLOG_INFO("thread1 is logging : %d",i);
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
+void log_thread2(){
+    int i = 0;
+    for (int j = 0; j < 10; j ++){
+        i ++;
+        mLOG_WARN("thread2 is logging : %d",i);
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    mLOG_DEBUG("haha %d",23);
+    std::thread t1(log_thread1);
+    std::thread t2(log_thread2);
+    if (t1.joinable())
+        t1.join();
+    if (t2.joinable())
+        t2.join();
     return 0;
 }
--- include/Logger.h
+++ include/Logger.h
@@ -8,17 +8,22 @@
 #include <ctime>
 #include <sstream>
 #include <iomanip>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <condition_variable>
+#include <atomic>
 
 #include "LogLevel.h"
 
-#define mLOG_INFO(format, ...) mTool::mLog::GetInstance().print(mTool::LogLevel::INFO,format,##__VA_ARGS__)
+#define mLOG_INFO(format, ...) mTool::mLog::GetInstance().print(mTool::LogLevel::INFO,format,##__VA_ARGS__);
 #ifdef DEBUG_ENV
-#define mLOG_DEBUG(format, ...) mTool::mLog::GetInstance().print(mTool::LogLevel::DEBUG,format,##__VA_ARGS__)
+#define mLOG_DEBUG(format, ...) mTool::mLog::GetInstance().print(mTool::LogLevel::DEBUG,format,##__VA_ARGS__);
 #else
-#define mLOG_DEBUG(format, ...) ((void)0)
+#define mLOG_DEBUG(format, ...) ((void)0);
 #endif
-#define mLOG_WARN(format, ...) mTool::mLog::GetInstance().print(mTool::LogLevel::WARN,format,##__VA_ARGS__)
-#define mLOG_ERROR(format, ...) mTool::mLog::GetInstance().print(mTool::LogLevel::ERROR,format,##__VA_ARGS__)
+#define mLOG_WARN(format, ...) mTool::mLog::GetInstance().print(mTool::LogLevel::WARN,format,##__VA_ARGS__);
+#define mLOG_ERROR(format, ...) mTool::mLog::GetInstance().print(mTool::LogLevel::ERROR,format,##__VA_ARGS__);
 
 namespace mTool{
 
@@ -32,17 +37,24 @@ public:
     template<class... Args>
     void print(LogLevel level, const char* format,Args... args){
         std::string msg = formatString(format,args...);
-        std::cout << getCurrentTimeString()
-                  << to_color(level)
-                  << to_string(level)
-                  << msg
-                  << "\033[0m"
-                  << std::endl;
+        msg = getCurrentTimeString() + to_color(level) + to_string(level)
+                + msg + "\033[0m";
+        {
+        std::lock_guard<std::mutex> lock(mtx_);
+        msg_.push(msg);
+        }
+        cv_.notify_one();
     }
 private:
-    mLog(){};                               // 构造函数
+    mLog(): running_(true), handle_msg_(&mLog::handle_msg,this){};                               // 构造函数
     mLog(const mLog&) = delete;             // 禁用拷贝
     mLog& operator=(const mLog&) = delete;  // 禁用传递
+    ~mLog(){
+        running_ = false;
+        cv_.notify_one();
+        if (handle_msg_.joinable())
+            handle_msg_.join();
+    }
 
     template<class... Args>
     std::string formatString(const char* format,Args... args){
@@ -68,6 +80,28 @@ private:
         oss << std::put_time(&tm_time, "[%Y-%m-%d %H:%M:%S]");
         return oss.str();
     }
+
+    void handle_msg(){
+        while (running_){
+            std::unique_lock<std::mutex> lock(mtx_);
+            cv_.wait(lock,[this](){return !msg_.empty() || !running_;});
+            if (msg_.empty())
+                break;
+            std::cout << msg_.front() << std::endl;
+            msg_.pop();
+        }
+        // 退出前清空剩余log
+        while (!msg_.empty()){
+            std::cout << msg_.front() << std::endl;
+            msg_.pop();
+        }
+    }
+
+    std::mutex mtx_;
+    std::queue<std::string> msg_;
+    std::condition_variable cv_;
+    std::atomic<bool> running_;
+    std::thread handle_msg_;
 };
 
 }
